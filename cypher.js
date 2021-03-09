1d701f6bcca1d1eeab1e0e99c3d40febe9fc4d9a
Anna Georgiadou <77627644+Annageorg@users.noreply.github.com>
2021-03-09 22:20:01 +0000

    functions done, working on html css

--- cypher.js
+++ cypher.js
@@ -1,20 +1,26 @@
-//[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
-//[65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
-//[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-//[97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
+// [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+// [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+// [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+// [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
 
-function encrypt (str, key){
+
+function encode (str, key){
     let message = "";
-    for (let i=0; i<str.length; i++){
+    for (let i = 0; i < str.length; i++){
         let ascii = str[i].charCodeAt();
-        if ( ascii >= 65 && ascii <= (90 - key) ){
-            message += String.fromCharCode(ascii + key);
-        } else if ( ascii > (90 - key)  && ascii <= 90 ){
-            message += String.fromCharCode(ascii - key);
-        } else if ( ascii >= 97 && ascii <= (122 - key) ){
-            message += String.fromCharCode(ascii + key);
-        } else if ( ascii > (122 - key)  && ascii <= 122 ){
-            message += String.fromCharCode(ascii - key);
+        let newAscii = ascii + key;
+        if (ascii >= 65 && ascii <=90){
+            if (newAscii <= 90) {
+                message += String.fromCharCode(newAscii);
+            } else {
+                message += String.fromCharCode(newAscii - 26);
+            }
+        } else if (ascii >= 97 && ascii <= 122){
+            if (newAscii <= 122) {
+                message += String.fromCharCode(newAscii);
+            } else {
+                message += String.fromCharCode(newAscii - 26);
+            }
         } else {
             message += str[i];
         }
@@ -22,18 +28,24 @@ function encrypt (str, key){
     return message;
 }
 
-function decrypt (str, key){
+function decode (str, key){
     let message = "";
-    for (let i=0; i<str.length; i++){
+    for (let i = 0; i < str.length; i++){
         let ascii = str[i].charCodeAt();
-        if ( ascii >= 65 && ascii <= (90 - key) ){
-            message += String.fromCharCode(ascii - key);
-        } else if ( ascii > (90 - key)  && ascii <= 90 ){
-            message += String.fromCharCode(ascii + key);
-        } else if ( ascii >= 97 && ascii <= (122 - key) ){
-            message += String.fromCharCode(ascii - key);
-        } else if ( ascii > (122 - key)  && ascii <= 122 ){
-            message += String.fromCharCode(ascii + key);
+        let newAscii = ascii - key;
+
+        if (ascii >= 65 && ascii <=90){
+            if (newAscii < 65){
+                message += String.fromCharCode(newAscii + 26)
+            } else {
+                message += String.fromCharCode(newAscii)
+            }
+        } else if (ascii >= 97 && ascii <= 122){
+            if (newAscii < 97){
+                message += String.fromCharCode(newAscii + 26)
+            } else {
+                message += String.fromCharCode(newAscii)
+            }
         } else {
             message += str[i];
         }
@@ -41,11 +53,6 @@ function decrypt (str, key){
     return message;
 }
 
-let input1 = "defend the East wall of the castle"
-let num = 1
-let output1 =  encrypt(input1, num)
-let input2 = "efgfoe uif Fbtu xbmm pg uif dbtumf"
-let output2 =  decrypt(input2, num)
-console.log(output1)
-console.log(output2)
+
+
 
