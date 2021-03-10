f376da7b3bf72f76ccac08527779988d4c452e19
Anna Georgiadou <77627644+Annageorg@users.noreply.github.com>
2021-03-10 17:39:19 +0000

    progress on the html and css

--- cypher.js
+++ cypher.js
@@ -4,6 +4,10 @@
 // [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
 
 
+// let key = document.getElementById("keys").selectedIndex.value;
+// let str = document.getElementById("plainTxt").value
+// document.getElementById("ccTxt").innerHTML = message;
+
 function encode (str, key){
     let message = "";
     for (let i = 0; i < str.length; i++){
@@ -33,7 +37,6 @@ function decode (str, key){
     for (let i = 0; i < str.length; i++){
         let ascii = str[i].charCodeAt();
         let newAscii = ascii - key;
-
         if (ascii >= 65 && ascii <=90){
             if (newAscii < 65){
                 message += String.fromCharCode(newAscii + 26)
